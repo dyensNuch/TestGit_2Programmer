a78c118e8c1d66d9f7c6ed6266286c059c11e508
dyensNuch <dyens@hotmail.com>
2016-06-11 15:37:38 +0700

    change data to int type

--- app/src/main/java/com/example/nuch_nb/testgit6/MainActivity.java
+++ app/src/main/java/com/example/nuch_nb/testgit6/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int pnuchData ="60";
+        int pnuchData =60;
+        //พี่นุชบอกว่า เฮ้ย มันเป็น int นิ เขียนผิด ๆ เลยแก้ซะเลย
         int kengData=35;
         //แล้วผมก็ add data ของผม และ commit  แต่ยังไม่ push นะ
 
